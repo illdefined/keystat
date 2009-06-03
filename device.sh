704abb213b7907d3e9efda0953d56ae2081df0f8
Mikael Voss <mvoss@tabularazor.org>
2009-06-03 14:12:38 +0200

    Use “grep -F” instead of “fgrep”.

--- device.sh
+++ device.sh
@@ -2,7 +2,7 @@
 
 for device in /sys/class/input/event*
 do
-	if fgrep -q keyboard "${device}/device/name"
+	if grep -F -q keyboard "${device}/device/name"
 	then
 		echo "/dev/input/$(basename ${device})"
 	fi
