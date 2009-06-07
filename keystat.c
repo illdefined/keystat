67f0a62d9d6fc4ce86c7ef376e9834bcc5259450
Mikael Voss <mvoss@tabularazor.org>
2009-06-07 20:06:50 +0200

    Zero-pad time difference in debug output.

--- keystat.c
+++ keystat.c
@@ -128,7 +128,7 @@ int main(int argc, char *argv[]) {
 				cur->avg = htonl(ntohl(cur->avg) - (ntohl(cur->avg) - df) / ntohl(cur->num));
 
 #ifdef DEBUG
-			printf("%.2hu %.2hu %.2hu: %uus\n",
+			printf("%.2hu %.2hu %.2hu: %.6uus\n",
 				ev[(idx + 1) % 3].code, ev[(idx + 2) % 3].code, ev[idx].code, df);
 #endif
 		}
