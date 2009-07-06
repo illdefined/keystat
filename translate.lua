c29bac59a0f6f4613a08abbf2e70482b09067867
Mikael Voss <mvoss@tabularazor.org>
2009-07-06 02:38:58 +0200

    Cleaned up indentation.

--- translate.lua
+++ translate.lua
@@ -1,6 +1,6 @@
 #!/usr/bin/env lua
 
-headerfile = "/usr/include/linux/input.h" 
+headerfile = "/usr/include/linux/input.h"
 
 f = io.open(headerfile)
 
@@ -16,32 +16,34 @@ end
 t = {}
 
 for line in f:lines() do
-   _, _, keyname, scancode = string.find(line, "^#define KEY_([^%s]+)%s+(%d+)")
-   if keyname and scancode then
-      t[scancode] = keyname
-   end
+	_, _, keyname, scancode = string.find(line, "^#define KEY_([^%s]+)%s+(%d+)")
+	if keyname and scancode then
+		t[scancode] = keyname
+	end
 end
 
 s = {}
 
 for line in dump:lines() do
-   _, _, key1, key2, key3, amount, latency = string.find(line, "(%d+)%s(%d+)%s(%d+)%s(%d+)%s(%d+)")
-   if key1 and key2 and key3 and amount and latency then
-      if tonumber(amount) > 0 then
-	 if t[key1] and t[key2] and t[key3] then
-	    triple = t[key1] .. " " .. t[key2] .. " " .. t[key3]
-	    entry = {}
-	    entry.triple = triple
-	    entry.amount = tonumber(amount)
-	    entry.latency = latency
-	    table.insert(s, entry)
-	 end
-      end
-   end
+	_, _, key1, key2, key3, amount, latency = string.find(line, "(%d+)%s(%d+)%s(%d+)%s(%d+)%s(%d+)")
+	if key1 and key2 and key3 and amount and latency then
+		if tonumber(amount) > 0 then
+			if t[key1] and t[key2] and t[key3] then
+				triple = t[key1] .. " " .. t[key2] .. " " .. t[key3]
+				entry = {}
+				entry.triple = triple
+				entry.amount = tonumber(amount)
+				entry.latency = latency
+				table.insert(s, entry)
+			end
+		end
+	end
 end
 
 table.sort(s, function(a,b) return a.amount > b.amount end)
 
 for _,triple in ipairs(s) do
-   print(triple.triple,triple.amount,triple.latency)
+	print(triple.triple,triple.amount,triple.latency)
 end
+
+-- vim:ts=4
