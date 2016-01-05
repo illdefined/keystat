91eb2cefc85033840b0818b5630ec35f92f5db3e
pajowu <github@ca.pajowu.de>
2016-01-05 18:35:08 +0200

    prevent Excetions when printing the final table, use space instead of tabs for indentation in translate.py

--- translate.py
+++ translate.py
@@ -9,20 +9,23 @@ keymap = subprocess.check_output(["xmodmap","-pke"]).decode("utf-8")
 mapping = {}
 
 for line in keymap.split("\n"):
-	parts = line.split()
-	if len(parts) > 4:
-		mapping[int(parts[1])+offset] = parts[3]
+    parts = line.split()
+    if len(parts) > 4:
+        mapping[int(parts[1])+offset] = parts[3]
 
 table = []
 
 for line in sys.stdin:
-	parts = line.split()
-	if int(parts[3]) > 0:
-		for position in positions_to_translate:
-			parts[position] = mapping[int(parts[position])]
-		table.append(parts)
+    parts = line.split()
+    if int(parts[3]) > 0:
+        for position in positions_to_translate:
+            parts[position] = mapping[int(parts[position])]
+        table.append(parts)
 
 table.sort(key=lambda x: int(x[3]), reverse=True)
 
 for row in table:
-	print(" ".join(row))
+    try:
+        print(" ".join(row))
+    except Exception:
+        break
