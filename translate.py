287b47fb63cec12d4832dfd47f2f89de0faf5cff
pajowu <github@ca.pajowu.de>
2016-01-05 18:21:42 +0200

    make translate.py executable

--- translate.py
+++ translate.py
@@ -2,15 +2,19 @@
 # -*- coding: utf-8 -*-
 import subprocess
 import sys
+
 positions_to_translate = [0,1,2]
 offset = -8
 keymap = subprocess.check_output(["xmodmap","-pke"]).decode("utf-8")
 mapping = {}
+
 for line in keymap.split("\n"):
 	parts = line.split()
 	if len(parts) > 4:
 		mapping[int(parts[1])+offset] = parts[3]
+
 table = []
+
 for line in sys.stdin:
 	parts = line.split()
 	if int(parts[3]) > 0:
@@ -19,6 +23,6 @@ for line in sys.stdin:
 		table.append(parts)
 
 table.sort(key=lambda x: int(x[3]), reverse=True)
+
 for row in table:
 	print(" ".join(row))
-#1,3
